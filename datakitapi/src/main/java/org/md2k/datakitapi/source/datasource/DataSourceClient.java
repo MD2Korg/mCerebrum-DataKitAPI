86438fd108458b89dcf34137e83a34f4c0eba222
monowar <smhssain@memphis.edu>
2015-11-13 23:26:02 -0600

    initial commit

--- datakitapi/src/main/java/org/md2k/datakitapi/source/datasource/DataSourceClient.java
+++ datakitapi/src/main/java/org/md2k/datakitapi/source/datasource/DataSourceClient.java
@@ -30,7 +30,7 @@ import java.io.Serializable;
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-public class DataSourceClient extends Object implements Serializable {
+public class DataSourceClient implements Serializable {
     private int ds_id;
     DataSource dataSource;
     private Status status;
