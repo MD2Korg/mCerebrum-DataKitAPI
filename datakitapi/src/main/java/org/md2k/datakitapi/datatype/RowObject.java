53458a4b3047f2490c6838690be17229a9569e03
Timothy W. Hnat <twhnat@memphis.edu>
2017-09-23 17:23:57 -0500

    Updates for CC uploader

--- datakitapi/src/main/java/org/md2k/datakitapi/datatype/RowObject.java
+++ datakitapi/src/main/java/org/md2k/datakitapi/datatype/RowObject.java
@@ -31,6 +31,9 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+
+import org.json.JSONObject;
 
 public class RowObject implements Parcelable{
     public static final Creator<RowObject> CREATOR = new Creator<RowObject>() {
@@ -97,5 +100,86 @@ public class RowObject implements Parcelable{
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeParcelable(data, flags);
         dest.writeLong(rowKey);
+    }
+
+    public String csvString() {
+        String result = String.valueOf(this.data.getDateTime());
+        result += "," + String.valueOf(this.data.offset);
+        
+        if (this.data instanceof DataTypeBoolean) {
+            result += "," + String.valueOf(((DataTypeBoolean) this.data).getSample());
+        }
+        if (this.data instanceof DataTypeBooleanArray) {
+            DataTypeBooleanArray d = (DataTypeBooleanArray) this.data;
+            for (boolean i : d.getSample()) {
+                result += "," + String.valueOf(i);    
+            }
+        }
+        if (this.data instanceof DataTypeByte) {
+            result += "," + String.format("%02X", ((DataTypeByte) this.data).getSample());
+        }
+        if (this.data instanceof DataTypeByteArray) {
+            DataTypeByteArray d = (DataTypeByteArray) this.data;
+            for (byte i : d.getSample()) {
+                result += "," + String.format("%02X", i);
+            }
+        }
+        if (this.data instanceof DataTypeDouble) {
+            result += "," + String.valueOf(((DataTypeDouble) this.data).getSample());
+        }
+        if (this.data instanceof DataTypeDoubleArray) {
+            DataTypeDoubleArray d = (DataTypeDoubleArray) this.data;
+            for (double i : d.getSample()) {
+                result += "," + String.valueOf(i);
+            }
+        }
+        if (this.data instanceof DataTypeFloat) {
+            result += "," + String.valueOf(((DataTypeFloat) this.data).getSample());
+        }
+        if (this.data instanceof DataTypeFloatArray) {
+            DataTypeFloatArray d = (DataTypeFloatArray) this.data;
+            for (float i : d.getSample()) {
+                result += "," + String.valueOf(i);
+            }
+        }
+        if (this.data instanceof DataTypeInt) {
+            result += "," + String.valueOf(((DataTypeInt) this.data).getSample());
+        }
+        if (this.data instanceof DataTypeIntArray) {
+            DataTypeIntArray d = (DataTypeIntArray) this.data;
+            for (int i : d.getSample()) {
+                result += "," + String.valueOf(i);
+            }
+        }
+        if (this.data instanceof DataTypeLong) {
+            result += "," + String.valueOf(((DataTypeLong) this.data).getSample());
+        }
+        if (this.data instanceof DataTypeLongArray) {
+            DataTypeLongArray d = (DataTypeLongArray) this.data;
+            for (long i : d.getSample()) {
+                result += "," + String.valueOf(i);
+            }
+        }
+        if (this.data instanceof DataTypeString) {
+            result += "," + ((DataTypeString) this.data).getSample();
+        }
+        if (this.data instanceof DataTypeStringArray) {
+            DataTypeStringArray d = (DataTypeStringArray) this.data;
+            for (String i : d.getSample()) {
+                result += "," + i;
+            }
+        }
+        if (this.data instanceof DataTypeJSONObject) {
+            result += "," + ((DataTypeJSONObject) this.data).getSample().toString();
+        }
+        if (this.data instanceof DataTypeJSONObjectArray) {
+            DataTypeJSONObjectArray d = (DataTypeJSONObjectArray) this.data;
+            for (JsonElement i : d.getSample()) {
+                result += "," + i.toString();
+            }
+        }
+
+        return result;
+
     }
 }
