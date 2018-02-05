b85dff67983e499f44eb46c22c4923ac0a548f12
Xavier Tilley <hippietilley@gmail.com>
2018-02-05 15:04:47 -0600

    MC-783 - Documented MessageType

--- datakitapi/src/main/java/org/md2k/datakitapi/messagehandler/MessageType.java
+++ datakitapi/src/main/java/org/md2k/datakitapi/messagehandler/MessageType.java
@@ -28,23 +28,72 @@
 package org.md2k.datakitapi.messagehandler;
 
 /**
+ * Defines integer value constants used to denote message types.
  *
+ * <p>
+ *     Message types are:
+ *     <ul>
+ *         <li><code>CONNECT</code></li>
+ *         <li><code>DISCONNECT</code></li>
+ *         <li><code>REGISTER</code></li>
+ *         <li><code>UNREGISTER</code></li>
+ *         <li><code>SUBSCRIBE</code></li>
+ *         <li><code>INSERT</code></li>
+ *         <li><code>QUERY</code></li>
+ *         <li><code>FIND</code></li>
+ *         <li><code>SUBSCRIBED_DATA</code></li>
+ *         <li><code>INTERNAL_ERROR</code></li>
+ *         <li><code>QUERYPRIMARYKEY</code></li>
+ *         <li><code>INSERT_HIGH_FREQUENCY</code></li>
+ *         <li><code>QUERYSIZE</code></li>
+ *         <li><code>SUMMARY</code></li>
+ *     </ul>
+ * </p>
  */
 public class MessageType {
 
+    /** Default is 1 */
     public static final int CONNECT = 1;
+
+    /** Default is 2 */
     public static final int DISCONNECT = 2;
+
+    /** Default is 3 */
     public static final int REGISTER = 3;
+
+    /** Default is 4 */
     public static final int UNREGISTER = 4;
+
+    /** Default is 5 */
     public static final int SUBSCRIBE = 5;
+
+    /** Default is 6 */
     public static final int UNSUBSCRIBE = 6;
+
+    /** Default is 7 */
     public static final int INSERT = 7;
+
+    /** Default is 8 */
     public static final int QUERY = 8;
+
+    /** Default is 9 */
     public static final int FIND = 9;
+
+    /** Default is 10 */
     public static final int SUBSCRIBED_DATA = 10;
+
+    /** Default is 11 */
     public static final int INTERNAL_ERROR = 11;
+
+    /** Default is 12 */
     public static final int QUERYPRIMARYKEY = 12;
+
+    /** Default is 13 */
     public static final int INSERT_HIGH_FREQUENCY = 13;
+
+    /** Default is 14 */
     public static final int QUERYSIZE = 14;
+
+    /** Default is 15 */
     public static final int SUMMARY = 15;
 }
